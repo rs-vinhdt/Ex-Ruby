32d79808004cf9960df4abe84200926ccfa5682c
RS-VinhDT <80232936+rs-vinhdt@users.noreply.github.com>
2022-03-16 11:50:00 +0700

    correcting

--- excercise2.rb
+++ excercise2.rb
@@ -12,3 +12,17 @@ end
 
 abc = Hash_Info.new name:"Thai Quan", age:22
 abc.displaying
+
+
+# VinhDT
+# class HashInfo 
+#   def initialize(name:, age:)
+#       @name = name
+#       @age = age
+#   end
+
+#   def displaying
+#       puts "name is: #@name"
+#       puts "age is: #@age"       
+#   end
+# end
